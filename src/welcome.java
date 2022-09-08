ed28c057cd4aac6c106e1f87beb0d7d79102aeca
Sarthak Goel <sarthakgoel@sarthak.goel>
2022-09-08 14:05:39 +0530

    second commit on master branch

--- src/welcome.java
+++ src/welcome.java
@@ -3,5 +3,6 @@ public class welcome {
     public static void main(String args[])
     {
         System.out.println("this is initial project");
+        System.out.println("this is second commit");
     }
 }
