31dfa6d7ff5053b20f1b47f7e445ed6bd2b1ce1c
Sarthak Goel <sarthakgoel@sarthak.goel>
2022-09-08 14:41:33 +0530

    created BugFix branch

--- src/welcome.java
+++ src/welcome.java
@@ -5,5 +5,7 @@ public class welcome {
         System.out.println("this is initial project");
         System.out.println("this is second commit");
         System.out.println("this is third commit");
+
+        System.out.println("this code is specific to bug fix");
     }
 }
